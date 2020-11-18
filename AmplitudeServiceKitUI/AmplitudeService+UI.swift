d49f3a55c2c3e206448dd0b522273beacd701894
Pete Schwamb <pete@schwamb.net>
2020-11-18 11:33:16 -0600

    Adding logo

--- AmplitudeServiceKitUI/AmplitudeService+UI.swift
+++ AmplitudeServiceKitUI/AmplitudeService+UI.swift
@@ -10,15 +10,27 @@ import SwiftUI
 import LoopKit
 import LoopKitUI
 import AmplitudeServiceKit
+import HealthKit
 
 extension AmplitudeService: ServiceUI {
+    
+    public static var image: UIImage? {
+        UIImage(named: "amplitude_logo", in: Bundle(for: AmplitudeServiceTableViewController.self), compatibleWith: nil)!
+    }
 
-    public static func setupViewController() -> (UIViewController & ServiceSetupNotifying & CompletionNotifying)? {
+    public static var providesOnboarding: Bool { return false }
+    
+    public static func setupViewController(currentTherapySettings: TherapySettings, preferredGlucoseUnit: HKUnit, chartColors: ChartColorPalette, carbTintColor: Color, glucoseTintColor: Color, guidanceColors: GuidanceColors, insulinTintColor: Color) -> (UIViewController & CompletionNotifying & ServiceSetupNotifying)?
+    {
         return ServiceViewController(rootViewController: AmplitudeServiceTableViewController(service: AmplitudeService(), for: .create))
     }
-
-    public func settingsViewController(chartColors: ChartColorPalette, carbTintColor: Color, glucoseTintColor: Color, guidanceColors: GuidanceColors, insulinTintColor: Color) -> (UIViewController & ServiceSettingsNotifying & CompletionNotifying) {
-      return ServiceViewController(rootViewController: AmplitudeServiceTableViewController(service: self, for: .update))
+    
+    public func settingsViewController(currentTherapySettings: TherapySettings, preferredGlucoseUnit: HKUnit, chartColors: ChartColorPalette, carbTintColor: Color, glucoseTintColor: Color, guidanceColors: GuidanceColors, insulinTintColor: Color) -> (UIViewController & CompletionNotifying & ServiceSettingsNotifying)
+    {
+        return ServiceViewController(rootViewController: AmplitudeServiceTableViewController(service: self, for: .update))
     }
-
+    
+    public func supportMenuItem(supportInfoProvider: SupportInfoProvider, urlHandler: @escaping (URL) -> Void) -> AnyView? {
+        return nil
+    }
 }
